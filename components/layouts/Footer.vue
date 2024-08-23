f564b27ac4a9940803d4275f896705f69afd11bc
bipmhh <github@vuongg.com>
2024-08-23 10:17:16 +0700

    Update Footer.vue

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -1,14 +1,3 @@
-<script setup>
-import { BloggerIcon, GitHubIcon, GmailIcon, MastodonIcon, TelegramIcon, XIcon } from 'vue3-simple-icons'
-
-const email = ref(null)
-onMounted(() => {
-  email.value.href = email.value.href.replace('$', '@')
-})
-</script>
-
 <template>
-  <section class="md:pt-6">
-    .
-  </section>
+
 </template>
