3b78af340e782de865a295f03ce1ea7183d1b63b
bipmhh <github@vuongg.com>
2024-08-23 10:10:04 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,19 @@
 
   </main>
 </template>
+<script>
+export default {
+  mounted () {
+    const user = this.$store.state.auth.user
+    if (user !== null) {
+      if (this.$route.query.link) {
+        window.location.href = this.$route.query.link
+      } else {
+        window.location.href = '/dashboard' // Some default link
+      }
+    } else {
+      window.location.href = '/dashboard' // login page
+    }
+  }
+}
+</script>
