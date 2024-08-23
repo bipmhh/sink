7d44a8140047516273fc5dcc64b7bcfedc31d982
bipmhh <github@vuongg.com>
2024-08-23 10:14:07 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,8 @@
 <template>
-  <main class="flex flex-col justify-center">
-
+  
+  <main class="flex items-center justify-center h-full">
+    <Login />
   </main>
+  
 </template>
-<script>
-export default {
-  mounted () {
-    const user = this.$store.state.auth.user
-    if (user !== null) {
-      if (this.$route.query.link) {
-        window.location.href = this.$route.query.link
-      } else {
-        window.location.href = '/dashboard' // Some default link
-      }
-    } else {
-      window.location.href = '/dashboard' // login page
-    }
-  }
-}
-</script>
+
